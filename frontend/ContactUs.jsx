2a07292a388616b2c172759ca81ccc9d376b8658
bokade <121025630+bokade@users.noreply.github.com>
2023-08-17 17:33:34 +0530

    Create ContactUs.jsx

---
+++ frontend/ContactUs.jsx
@@ -0,0 +1,47 @@
+
+import Sumeet from "../images/Sumeet.jpeg";
+import Vishal from "../images/vshinde.jpg";
+import Zavare from "../images/Zavare.jpg";
+import Simran from "../images/Simran.jpg";
+import Vaibhav from "../images/Vaibhav.jpg";
+import Swapnil from "../images/Swapnil.jpg";
+import Sourabh from "../images/Sourabh.jpg";
+import Chinmay from "../images/Chinmay.jpg";
+
+
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+        <h1> <center>We are here for you !</center></h1>
+        <h2> <center>Our team of experts is on hand to answer your questions</center></h2>
+        <br />
+        <br />
+
+        <br />
+        <center>
+        <h4>Can't find the solution you are looking for? Here's how to get help from our experts</h4>
+        <br />
+        <p>Connect with our experts:<br></br>
+      
+        <br />
+          
+        <br />
+        <a href="https://www.linkedin.com/in/sumeet-alhat-879583183/" target="_blank"><img src={Sumeet} alt="Sumeet Image" height={150} width={120}/></a>                <a href="https://www.linkedin.com/in/vishalpshinde" target="_blank"> <img src={Vishal} alt="Vishal Image" height={150} width={120}/></a>                   <a href="https://www.linkedin.com/in/sumedh-zavare-4b1722194" target="_blank"> <img src={Zavare} alt="Sumedh Image" height={150} width={120}/></a>                        <a href="https://www.linkedin.com/in/vaibhavkale321/" target="_blank"> <img src={Vaibhav} alt="Vaibhav Image" height={150} width={120}/></a>                   <a href="https://www.linkedin.com/in/simran-shaikh-037848254" target="_blank"> <img src={Simran} alt="Simran Image" height={150} width={120}/></a>                                <a href="https://www.linkedin.com/in/swapnil-bokade-585a341aa" target="_blank"><img src={Swapnil} alt="Swapnil Image" height={150} width={120}/></a>                                      <a href="https://www.linkedin.com/in/sourav-shinde-4930b6262" target="_blank"><img src={Sourabh} alt="Sourabh Image" height={150} width={120}/></a>                                     <a href="#" target="_blank"><img src={Chinmay} alt="Chinmay Image" height={150} width={120}/></a>     
+
+
+        
+        </p>
+        </center>
+
+        
+        
+        
+
+        
+      </b>
+    </div>
+  );
+};
+
+export default ContactUs;
